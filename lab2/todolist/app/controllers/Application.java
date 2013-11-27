e8d4154b377aec129da26c68b49449f4d4582f37
acacioleal <acacio.leal@ccc.ufcg.edu.br>
2013-11-27 01:10:08 -0200

    implementacao pronta

--- lab2/todolist/app/controllers/Application.java
+++ lab2/todolist/app/controllers/Application.java
@@ -6,6 +6,11 @@ import play.data.Form;
 import play.mvc.*;
 import views.html.*;
 
+/**
+ * 
+ * Classe que realiza a aplicacao.
+ *
+ */
 public class Application extends Controller {
 	
 	static Form<Task> taskForm = Form.form(Task.class);
@@ -14,10 +19,16 @@ public class Application extends Controller {
 	    return  redirect(routes.Application.tasks());
 	  }
 	  
+	/**
+	 * @return tasks
+	 */
 	  public static Result tasks() {
 		  return ok(views.html.index.render(Task.all(), taskForm));
 	  }
 	  
+	  /**
+	   * @return uma nova task criada.
+	   */
 	  public static Result newTask() {
 		  Form<Task> filledForm = taskForm.bindFromRequest();
 		  if(filledForm.hasErrors()) {
@@ -27,12 +38,19 @@ public class Application extends Controller {
 		    return redirect(routes.Application.tasks());  
 		  }
 	  }
-	  
+	  /**
+	   * @param id
+	   * @return deleta a task
+	   */
 	  public static Result deleteTask(Long id) {
 		  Task.delete(id);
 		  return redirect(routes.Application.tasks());
 	  }
 	  
+	  /**
+	   * @param id
+	   * @return faz um update na tarefa. (marca como feita)
+	   */
 	  public static Result update(Long id) {
 		  Task.update(id);
 		  return redirect(routes.Application.tasks());
--- lab2/todolist/app/models/Task.java
+++ lab2/todolist/app/models/Task.java
@@ -7,6 +7,11 @@ import play.data.validation.Constraints.*;
 
 import javax.persistence.*;
 
+/**
+ * 
+ * Classe das tarefas.
+ *
+ */
 @Entity
 public class Task extends Model {
 
@@ -28,6 +33,11 @@ public class Task extends Model {
 
 public static Finder<Long,Task> find = new Finder(Long.class, Task.class);
 
+/**
+ * 
+ * @return Lista das tarefas ordenadas.
+ */
+
   public static List<Task> all() {
           List<Task> bdList = find.all();
           Comparator<Task> comparador = new Comparator<Task>() {
@@ -40,14 +50,31 @@ public static Finder<Long,Task> find = new Finder(Long.class, Task.class);
           return bdList;
   }
 	
-
+  	/**
+  	 * 
+  	 * @param task
+  	 * Cria e salva uma tarefa.
+  	 */
+  
 	public static void create(Task task) {
 	  task.save();
 	}
-
+	
+	/**
+	 * 
+	 * @param id
+	 * Deleta uma tarefa criada.
+	 */
+	
 	public static void delete(Long id) {
 	  find.ref(id).delete();
 	}
+	
+	/**
+	 * 
+	 * @param id
+	 * Marca a tarefa como realizada.
+	 */
 	
 	public static void update(Long id) {
 		Task task = (Task) find.ref(id);
@@ -55,35 +82,74 @@ public static Finder<Long,Task> find = new Finder(Long.class, Task.class);
         task.update();
 	}
 
+	/**
+	 * @return Nome da variavel.
+	 * 
+	 */
 	
 	public String getNome() {
 		 return nome;
 	}
 
+	/**
+	 * @param nome
+	 * Modifica o nome da variavel.
+	 */
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
+	/**
+	 * @return Descricao da variavel.
+	 * 
+	 */
+	
 	public String getDescricao() {
 		return descricao;
 	}
 
+	/**
+	 * @param descricao
+	 * Modifica a descricao da variavel.
+	 */
+	
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+	/**
+	 * @return prioridade da variavel.
+	 * 
+	 */
+	
 	public int getPrioridade() {
 		return prioridade;
 	}
-
+	/**
+	 * @param prioridade
+	 * Modifica a prioridade da variavel.
+	 */
+	
 	public void setPrioridade(int prioridade) {
 		this.prioridade = prioridade;
 	}
+	
+	/**
+	 * @return Se a variavel foi feita ou nao.
+	 * 
+	 */
 	
 	public boolean isDone() {
 		return done;
 	}
 
+	/**
+	 * 
+	 * @param done
+	 * Modifica a condicao da variavel.
+	 */
+	
 	public void setDone(boolean done) {
 		this.done = done;
 	}
